8e34f1c5e35548bf7eb76d13aef8e4c826a491ae
Jonny <sherzod.baratov.1991@mail.ru>
2024-01-31 18:29:51 +1300

    feat:to develop router process

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const http = require("http");
 const mongodb = require("mongodb");
 
-let db;
-const connectionString =
-  "mongodb+srv://todouser:todo14@cluster0.b91ez.mongodb.net/Papays?authSource=admin&replicaSet=atlas-1371sn-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+const connectionString = process.env.MONGO_URL;
+
 
 mongodb.connect(
   connectionString,
@@ -19,7 +21,7 @@ mongodb.connect(
 
       const app = require("./app");
       const server = http.createServer(app);
-      let PORT = 3003;
+      let PORT = process.env.PORT || 3003;
       server.listen(PORT, function () {
         console.log(
           `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
