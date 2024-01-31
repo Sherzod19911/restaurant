1e2e0cd64648de4ff15d28b93cc46800710013e5
Jonny <sherzod.baratov.1991@mail.ru>
2024-02-01 06:30:20 +1300

    feat: bcrypts package asosida paswordni hash qilish

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -21,21 +21,45 @@ memberController.signup = async (req, res) => {
   const data = req.body; //requestni badiy qismidan malumot olamiz.
   //console.log(`body:::`,req.body);
   const member = new Member();
-  const new_member = await member.signupData(data);
+  //const new_member = await member.signupData(data);
   
-  res.send("done");
+
+  
+  
+
+ new_member = await member.signupData(data);
+console.log("i am here1991");
+res.json({state: "succeed", data:new_member});
+//res.send("done");
   } catch(err) {
   console.log(`ERROR, cont/signup, ${err.message}`);
+  res.json({state: 'fail',message: err.message});
   };
 }
 
 
-memberController.login = (req, res) => {
-  console.log("POST cont.login");
-  res.send("login sahifadasiz");
-};
+// memberController.login = (req, res) => {
+//   console.log("POST cont.login");
+//   res.send("login sahifadasiz");
+// };
+
+memberController.login = async(req, res) => {
+  try{
+    console.log("POST:cont/login");
+    const data = req.body, //requestni badiy qismidan malumot olamiz.
+    //console.log(`body:::`,req.body);
+    member = new Member(),
+    result= await member.loginData(data);
+    //console.log(1991);
+    res.json({state: 'succeed', data:result});
+    //res.send("done");
+    } catch(err) {
+    console.log(`ERROR, cont/login, ${err.message}`);
+    res.json({state: 'fail',message: err.message});
+    }
 
 memberController.logout = (req, res) => {
   console.log("GET cont.logout");
   res.send("logout sahifadasiz");
+}
 }
