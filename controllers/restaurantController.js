f08c1609c4d739236cf0853c58fd280f063d71fa
Jonny <sherzod.baratov.1991@mail.ru>
2024-02-04 00:43:57 +1300

    feat: develop sessions and login pages

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,6 +1,17 @@
 const Member = require("../models/Member");
 
 let restaurantController = module.exports;
+
+restaurantController.getMyRestaurantData = async (res, req) => {
+  try {
+      console.log("GET:cont/getMyRestaurantData");
+      res.render('restaurant-menu');
+  }catch (err) {
+      console.log(`ERROR, cont/getMyRestaurantData, ${err.message}`);
+      res.json({state: 'fail',message: err.message});   
+  }
+}
+
 restaurantController.getSignupMyRestaurant = async (res, req) => {
     try {
         console.log("GET:cont/getSignupMyRestaurant");
@@ -18,9 +29,15 @@ const data = req.body, //requestni body qismidan malumot olamiz.
 
 member = new Member(),
 
+
+
  new_member =  await member.signupData(data);
+ req.session.member = new_member;
+ res.redirect("/resto/products/menu");
+
+
 console.log("new member:",new_member );
-res.json({state: "succeed", data:new_member});
+//res.json({state: "succeed", data:new_member});
 //res.send("done");
 } catch(err) {
 console.log(`ERROR, cont/signup, ${err.message}`);
@@ -30,12 +47,13 @@ res.json({state: 'fail',message: err.message});
 restaurantController.getLoginMyRestaurant = async (res, req) => {
     try {
         console.log("GET:cont/getLoginMyRestaurant");
-        res.rander('login-page');
+        res.render('login-page');
     }catch (err) {
         console.log(`ERROR, cont/getSignupMyRestaurant, ${err.message}`);
         res.json({state: 'fail',message: err.message});   
     }
 }
+
 
 
 restaurantController.loginProcess = async(req, res) => {
@@ -45,8 +63,13 @@ restaurantController.loginProcess = async(req, res) => {
     //console.log(`body:::`,req.body);
     member = new Member(),
     result= await member.loginData(data);
+    req.session.member = result;
+    req.session.save(function () {
+        res.redirect("/resto/products/menu");
+
+    });
     console.log(1991);
-    res.json({state: 'succeed', data:result});
+   // res.json({state: 'succeed', data:result});
     //res.send("done");
     } catch(err) {
     console.log(`ERROR, cont/login, ${err.message}`);
@@ -58,3 +81,10 @@ console.log("shrzod");
   console.log("GET cont.logout");
   res.send("logout sahifadasiz");
 };
+restaurantController.checkSessions = (req,res) => {
+    if (req.session?.member) {
+      res.json({state: "succeed", data: req.session.member});
+    } else {
+      res.json({state: "fail", message: "You are not authenticated"});
+    } 
+}
